8031fc59af473e7098728e04243d3332c6256735
taquang.nhut <taquangnhut7@gmail.com>
2026-09-23 09:36:45 +0700

    finish video 19

---
+++ src/video19/video19.js
@@ -0,0 +1,14 @@
+console.log("video 19")
+
+//object key value
+
+const myperson = {
+    name: "kieu",
+    age : 20,
+    address: "quangnam"
+}
+
+//array
+
+const scores = [9 , 9.1, 5 , 7.5]
+console.log(scores)
