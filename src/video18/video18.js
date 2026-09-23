50104f93cfd048a52b8d2297cea6bdae273fadbe
taquang.nhut <taquangnhut7@gmail.com>
2026-09-23 09:22:35 +0700

    finish video 17 18

---
+++ src/video18/video18.js
@@ -0,0 +1,18 @@
+console.log("video 18")
+
+
+//string
+const name = "kieu"
+const name1 = "nhut"
+const name2 = "quangnhut"
+console.log(name)
+
+//number
+const myAge = 25;
+const myScore = 9.5;
+console.log(myAge)
+
+//boolean true/false
+
+const istoiDeptrai = true;
+console.log(istoiDeptrai)
