1deb8e5ca2e3e00db2988758a6004dd93429aa73
taquang.nhut <taquangnhut7@gmail.com>
2026-09-23 11:20:58 +0700

    finish video 21

---
+++ src/video21/video21.js
@@ -0,0 +1,10 @@
+console.log("video 21")
+
+const name = " nhut"
+const age = 20
+
+const introduction = "my name is" + name + "my age is " + age
+console.log(introduction)
+
+const intro = `my name is ${name} my age is ${age}`
+console.log(intro)
