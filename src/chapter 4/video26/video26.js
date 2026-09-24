9d7309c7af5a5bd55aec76af44f1e189d7876ad3
taquang.nhut <taquangnhut7@gmail.com>
2026-09-24 10:22:32 +0700

    finish video 26

---
+++ src/chapter 4/video26/video26.js
@@ -0,0 +1,10 @@
+console.log("video 26")
+
+const score = 4;
+if (score >= 8 && score <= 10) {
+    console.log("hoc sinh gioi")
+} else if (score >= 6 && score < 8) {
+    console.log("hoc sinh kha")
+} else {
+    console.log("hoc sinh trung binh")
+}
